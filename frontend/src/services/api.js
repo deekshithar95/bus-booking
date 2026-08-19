77557cf2e2d65f7510f2f91036db2c5494e7c900
deekshithar95 <deekshithar7204@gmail.com>
2026-08-19 11:45:32 +0530

    Dockerize bus booking application

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,27 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "/api",
+
   headers: {
     "Content-Type": "application/json"
   }
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
 
 export default api;
